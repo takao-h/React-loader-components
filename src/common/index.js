4ed874790dda1f1b00e8f5c492955cce2f0c47b9
takao-h <hayashi_takao@cyberbuzz.co.jp>
2018-04-06 00:26:57 +0900

    add load html

--- src/common/index.js
+++ src/common/index.js
@@ -1,1 +1,13 @@
 export { load } from "./load";
+const loader = (message) => {
+    <div>
+      <div class="cp_loading16">
+	    <div class="cp_item"></div>
+	    <div class="cp_item"></div>
+	    <div class="cp_item"></div>
+      </div>
+      <p>{message}</p>
+    </div>
+}
+
+export default {loader}
