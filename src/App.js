a0f5a0860f4623610a03fc0f7108bc9cd258e16b
takao-h <hayashi_takao@cyberbuzz.co.jp>
2018-04-08 22:42:45 +0900

    add css

--- src/App.js
+++ src/App.js
@@ -6,23 +6,25 @@ class App extends Component {
   render() {
     const loader = (message) => {
       <div>
-        <div class="cp_loading16">
-        <div class="cp_item"></div>
-        <div class="cp_item"></div>
-        <div class="cp_item"></div>
+        <div class="cp_loading17">
+          <div class="cp_item"></div>
+          <div class="cp_item"></div>
+          <div class="cp_item"></div>
+          <div class="cp_item"></div>
+          <div class="cp_item"></div>
         </div>
         <p>{message}</p>
       </div>
   }
-  
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+
+  return(
+      <div className = "App" >
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="App-title">Welcome to React</h1>
+      </header>
+      <p className="App-intro">
+        To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
     );
