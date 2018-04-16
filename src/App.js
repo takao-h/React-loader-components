302b8b866b795ca0538b3f0de675ca67ae88580b
takao-h <hayashi_takao@cyberbuzz.co.jp>
2018-04-16 21:58:26 +0900

    Add message area

--- src/App.js
+++ src/App.js
@@ -1,32 +1,28 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import './loader.css'
+import './loader.css';
 
 class App extends Component {
-  render() {
-  return(
-      <div className = "App" >
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="App-title">Welcome to React</h1>
-      </header>
-      <p className="App-intro">
-        To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <div>
-          <div className="cp_loading17">
-            <div className="cp_item"></div>
-            <div className="cp_item"></div>
-            <div className="cp_item"></div>
-            <div className="cp_item"></div>
-            <div className="cp_item"></div>
-          </div>
-          <p>hello!!</p>
-        </div>
-      </div>
-    );
-  }
+    render() {
+        return(
+            <div className = "App" >
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                </header>
+                <div>
+                    <div className="cp_loading17">
+                        <div className="cp_item"></div>
+                        <div className="cp_item"></div>
+                        <div className="cp_item"></div>
+                        <div className="cp_item"></div>
+                        <div className="cp_item"></div>
+                        <p>now loading !!!!!!!</p>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
