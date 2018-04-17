9e4d47d8fe1e83e322ceda90b29bddee25635f5c
takao-h <hayashi_takao@cyberbuzz.co.jp>
2018-04-18 00:55:17 +0900

    Add new component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,21 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import './loader.css';
+import BoxSlide from './loading';
 
 class App extends Component {
-    render() {
-        return(
-            <div className = "App" >
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                </header>
-                <div>
-                    <div className="cp_loading17">
-                        <div className="cp_item"></div>
-                        <div className="cp_item"></div>
-                        <div className="cp_item"></div>
-                        <div className="cp_item"></div>
-                        <div className="cp_item"></div>
-                        <p>now loading !!!!!!!</p>
-                    </div>
-                </div>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="App" >
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+        <div>
+          <BoxSlide />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
