dcd10f5b5aebfc16b8266178455e8c6615363aa7
takao-h <hayashi_takao@cyberbuzz.co.jp>
2018-04-18 13:02:20 +0900

    fix conflict

--- src/App.js
+++ src/App.js
@@ -1,41 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import BoxSlide from './loading';
 
 class App extends Component {
-<<<<<<< HEAD
     render() {
         return(
             <div className = "App" >
-
-                <div>
-                    <div className="cp_loading17">
-                        <div className="cp_item"></div>
-                        <div className="cp_item"></div>
-                        <div className="cp_item"></div>
-                        <div className="cp_item"></div>
-                        <div className="cp_item"></div>
-                        <p>now loading!!!!!!!</p>
-                    </div>
-                </div>
+               <BoxSlide />
             </div>
         );
     }
-=======
-  render() {
-    return (
-      <div className="App" >
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-        </header>
-        <div>
-          <BoxSlide />
-        </div>
-      </div>
-    );
-  }
->>>>>>> 9e4d47d8fe1e83e322ceda90b29bddee25635f5c
 }
 
 export default App;
