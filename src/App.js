0ebe0aca7e6ef70b19f256882b90659546a9cae9
takao-h <hayashi_takao@cyberbuzz.co.jp>
2018-04-30 18:25:23 +0900

    fix color for loading compoments

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import BoxSlide from './loading/loading';
+import Loader2 from './loading/Loader2';
+ 
+
 class App extends Component {
     render() {
         return(
             <div className = "App" >
-               <BoxSlide />
+               <Loader2 />
             </div>
         );
     }
